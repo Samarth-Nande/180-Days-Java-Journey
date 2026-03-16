8cf23caf1c30c1ba0fb4121b06d07ac6cbe967ce
samarth-20 <samarthn097@gmail.com>
2026-03-16 23:10:13 +0530

    Added day 13 programs

---
+++ src/Day 13/copy_arr_elements.java
@@ -0,0 +1,19 @@
+package Day 13;
+
+public class copy_arr_elements {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = new int[arr1.length];
+        
+        // Copying elements from arr1 to arr2
+        for(int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+        
+        // Printing the copied array
+        System.out.print("Copied Array: ");
+        for(int num : arr2) {
+            System.out.print(num + " ");
+        }
+    }
+}
